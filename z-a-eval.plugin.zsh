430ebca6d96e1fb013f8dafe456c1d7e11b2c264
Salvydas Lukosius <sall@w-ss.io>
2021-11-30 11:58:05 +0000

    modified:   .gitignore 	modified:   :za-ev-atclone-handler 	modified:   :za-ev-atinit-handler 	modified:   :za-ev-recache 	modified:   README.md 	renamed:    _zinit_recache -> _zi_recache 	modified:   z-a-eval.plugin.zsh

--- z-a-eval.plugin.zsh
+++ z-a-eval.plugin.zsh
@@ -13,47 +13,47 @@ autoload :za-ev-atclone-handler :za-ev-atinit-handler :za-ev-recache
 :za-ev-null-handler() { :; }
 
 # Remove temporary variable
-:za-ev-atload-handler() { [[ -n ${ICE[eval]} ]] && unset ZINIT_Z_A_EVAL_SOURCED; }
+:za-ev-atload-handler() { [[ -n ${ICE[eval]} ]] && unset ZI_Z_A_EVAL_SOURCED; }
 
-@zinit-register-annex "z-a-eval" \
+@zi-register-annex "z-a-eval" \
     hook:atclone-50 \
     :za-ev-atclone-handler \
     :za-ev-null-handler \
     "eval''" # also register new ices
 
-@zinit-register-annex "z-a-eval" \
+@zi-register-annex "z-a-eval" \
     hook:atpull-50 \
     :za-ev-atclone-handler \
     :za-ev-null-handler
 
-@zinit-register-annex "z-a-eval" \
+@zi-register-annex "z-a-eval" \
     hook:atload-50 \
     :za-ev-atload-handler \
     :za-ev-null-handler
 
-@zinit-register-annex "z-a-eval" \
+@zi-register-annex "z-a-eval" \
     hook:atinit-50 \
     :za-ev-atinit-handler \
     :za-ev-null-handler
 
-@zinit-register-annex "z-a-eval" \
+@zi-register-annex "z-a-eval" \
     subcommand:recache \
     :za-ev-recache \
     :za-ev-null-handler
 
 (( Z_A_USECOMP )) || return;
 
-# Annex provides a completion file with the prefix _zinit
-# Annex provies the 'shim' below which will run all available zinit completions
-# Lastly the shim is assigned as zinits completion with a compdef call
-autoload -Uz _zinit_recache
-_zinit_shim(){
+# Annex provides a completion file with the prefix _zi
+# Annex provies the 'shim' below which will run all available zi completions
+# Lastly the shim is assigned as zis completion with a compdef call
+autoload -Uz _zi_recache
+_zi_shim(){
   unset -f $funcstack[1]
   eval "
     $funcstack[1](){
-      ${(F)${(@ok)functions[(I)_zinit*]/%/ \"\$@\"}//$funcstack[1] \"\$@\"}
+      ${(F)${(@ok)functions[(I)_zi*]/%/ \"\$@\"}//$funcstack[1] \"\$@\"}
     }
     eval $funcstack[1] \$@
   "
 }
-compdef _zinit_shim zinit
+compdef _zi_shim zi
